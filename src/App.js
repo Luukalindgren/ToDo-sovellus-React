1e44e170131d9261aa049501e2a30aa3e093738b
Luuka Lindgren <luslin@utu.fi>
2022-02-02 13:30:31 +0200

    Tehtävien yliviivaus

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
-//TEHTÄVÄNANTO VAIHEET:
+//TODO:
 //1. VALMIS!
-//2. KESKEN, klikkaus yliviivaa kaikki nykyiset ja tulevat notet, eikä uudellen klikkaus palauta...
-//3. VALMIS!
-//4. VALMIS, ongelmana on se, ettei lista päivity vasta kun kirjoittaa jotain inputtiin...
+//2. VALMIS!
+//3. VALMIS! KORJATTAVA YHTEENSOPIVAKSI YLIVIIVAUKSEN KANSSA!
+//4. VALMIS!
 //5. VALMIS!
 
 
 import './App.css';
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 
 
 function App () {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(["Tässä todo -sovelluskessa on vikoja joille en keksinyt ratkaisua...","Lajittelu ja tehdyksi merkkaus ei uudelleen renderöi listaa...","Syötekenttään tarvitsee laittaa jokin merkki renderöityäkseen.", "Tehdyksi merkkauksen poisto ei toimi oikein..."]);
   const [value, setValue] = useState("");
-  const [style, setStyle] = useState("eiTehty");
+  const [tehdyt, setTehdyt] = useState([]);
 
   function addToNotes() {     //Lisää listan(notes) viimeiseksi halutun noten
     if(value === ""){
       alert("Muistiinpano ei voi olla tyhjä!")
     }else{
-      let tempArr = notes;
-      tempArr.push(value);
-      setNotes(tempArr);
+      let temp = notes;
+      temp.push(value);
+      setNotes(temp);
       setValue("");
     }
-
 }
 
   function poistaNote(index) {  //Poistaa valitun noten sen indeksin mukaan
     let temp = notes.filter((item, i) => i !== index);
     setNotes(temp);
+    //KORJATTAVAA ON YLIVIIVAUKSEN SIIRTO, NYT KUN POISTAA JONKUN, NIIN YLIVIIVAUS PYSYY SAMASSA INDEKSISSÄ, VAIKKA PITÄISI PIENENTYÄ YHDELLÄ/POISTUA!
   }
   
   function yliviivaaNote(index) {
-    //KEKSI RATKAISU!
-    setStyle("tehty") //setStylellä klikkaamalla saadaan vaihdettua kaikkien muistiinpanojen luokka, uudestaan klikkaus ei palauta...
-    console.log({index});
+    //Jos tehdyt -listassa on jo tämä tehdyn indexi, se poistetaan, jolloin kyseisen yliviivauspoistuu
+    if (tehdyt.includes(index)) {
+      let temp = tehdyt.filter((item) => item !== index);
+      setTehdyt(temp);
+    } //Jos ei ole, niin tehdyt -listaan lisätään indexi, jolloin sen tyyliin tulee yliviivaus lisää.
+    else {
+      let temp = tehdyt;
+      temp.push(index);
+      setTehdyt(temp);
     }
+  }
 
-  function lajittele() {
-    //Pelkkä notes.sort() järjestää sen A-Ö, mutta se ei renderöi automaattisesti listaa...
-    let temp = notes.sort();
-    setNotes([""]);
-    console.log(notes);
-    setNotes(temp);
-    setValue("");
+  function lajittele() {     
+    setNotes(notes.sort());
+    // JOSTAIN SYYSTÄ INPUTTIIN PITÄÄ LAITTAA JOKIN SYMBOLI ENNENKUIN LISTA PÄIVITTYY
+  }
 
-    console.log(notes);
+  function poistaKaikki() {
+    setNotes([]);
+    setTehdyt([]); //Yliviiaukset poistuu ettei ne tule uusiin merkintöihin mukaan
   }
 
 
@@ -58,14 +64,15 @@ function App () {
         onChange={(e) => setValue(e.target.value)}/>
       <button id='lisää' onClick={addToNotes}> + </button>
       <ul className='allNotes'>
-        {notes.map((item, i) => <li className={style} id={i} key={i} onClick={() => yliviivaaNote(i)}>{item} 
+        {notes.map((item, i) => <li  className={tehdyt.includes(i) ? 'tehty' : ''} id={i} key={i} >{item}   
+        {/* ClassName vaihtuu jokaiselle listan alkiolle, riippuen kuuluuko niiden indeksi tehdyt -listaan */}
+        <button id='yliviivaa' onClick={() => yliviivaaNote(i)}> ✓ </button>
         <button id='poista' onClick={() => poistaNote(i)}> X </button>
         </li>)}
       </ul>
-
       <div>
-        <button id="lajittele" onClick={lajittele}>Lajittele A-Ö</button>
-        <button id="poistakaikki" onClick={() => setNotes([])} >Poista kaikki</button>
+        <button id="lajittele" onClick={lajittele} >Lajittele A-Ö</button>
+        <button id="poistakaikki" onClick={poistaKaikki} >Poista kaikki</button>
       </div>
     </div>
     
